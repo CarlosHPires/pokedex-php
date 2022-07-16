6f5c7ff200aac07c33b9e0985c29561cb37bf9bd
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2022-07-16 02:53:21 -0300

    feat(route): I just created the route system

---
+++ config/config.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./styles/nav.css" />
+    <link rel="stylesheet" type="text/css" href="./styles/global.css" />
+    <title>Poké Fire</title>
+</head>
+<body>
+    <header>
+        <nav>
+        <a class="logo" href="?router=Site/home/">Poké Fire</a>
+        <div class="mobile-menu">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+            <div class="line4"></div>
+        </div>
+        <ul class="nav-list">
+            <li><a href="?router=Site/home/">Home</a></li>
+            <li><a href="?router=Site/about">About</a></li>
+            <li><a href="?router=Site/pokedex">Pokédex</a></li>
+            <li><a href="?router=Site/query">Pokémons</a></li>
+        </ul>
+        </nav>
+    </header>
+    <script src="./js/nav.js"></script>
+</body>
+</html>
